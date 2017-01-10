d0b4de1642d581430eae93892426fd484f899bce
Matthew Marks <matthewfmarks@gmail.com>
2017-01-10 10:57:46 -0500

    replace get-result[s] with search->single-download-link

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -5,7 +5,9 @@
             [clj-http.client :as client]
             [clojure.java.jdbc :as j]
             ;; [mysql-connector-java]
+            [libby.db :as db]
             libby.core))
+
 
 (defn start []
   (mount/start-without #'libby.core/http-server
@@ -41,56 +43,4 @@
     (with-open [w (io/output-stream (str "pdfs/" (req->file-name req)))]
       (.write w (:body req)))
     req))
-
-(defn get-results [query]
-  (let [search-body (:body (client/get (str "http://libgen.io/search.php?req=" query)))
-        ads (re-seq #"ads.php[^']+" search-body)
-        download-bodies (map #(:body (client/get (str "http://libgen.io/" %))) ads)
-;;         ad (first ads)
-        ;; page-body (:body (client/get (str "http://libgen.io/" ad)))
-        direct-links (map  #(str "http://libgen.io/" (re-find #"get\.php[^']+"  %)) download-bodies)]
-    direct-links))
-
-(defn query->download-link [query]
-  (let [search-body (:body (client/get (str "http://libgen.io/search.php?req=" query)))
-        md5s (distinct (re-seq #"(?<=md5=).{32}" search-body))
-        download-links (map (str "http://libgen.io/get/ %" md5s))
-        ]))
-
-(defn get-result [query]
-  (let [search-body (:body (client/get (str "http://libgen.io/search.php?req=" query)))
-        ads (re-seq #"ads.php[^']+" search-body)
-        ad (first ads)
-        download-body (:body (client/get (str "http://libgen.io/" ad)))
-        direct-link (str "http://libgen.io/" (re-find #"get\.php[^']+"  download-body))]
-    direct-link))
-
-
-(def mysql-db {:dbtype "mysql"
-               :dbname "bookwarrior"
-               :user "root"
-               :password ""
-               })
-
-(defn- map->download-link [{:keys [md5 author title extension] :as m}]
-  (str "http://libgen.io/get/" md5 "/" author "-" title "." extension))
-
-(defn- remove-empties [m] ;; http://stackoverflow.com/a/3938151
-  (into {} (remove (comp #(= "" %) second) m)))
-
-(defn md5->list-of-maps [md5]
-  (first (j/query mysql-db ["select title,Extension,Filesize from updated where md5 = ?" md5])))
-
-
-(defn map->map-with-download-link [m]
-  (assoc m :download-link (map->download-link m)))
-
-(defn big-map->download-links [big-map]
-  (map map->download-link big-map))
-
-(defn search->big-map [search]
-  (let [big-map-with-empties (j/query mysql-db ["select * from updated where title like ?" (str search "%")])
-        big-map (map remove-empties big-map-with-empties)
-        big-map-with-download-links (map map->map-with-download-link big-map)]
-    big-map-with-download-links))
 
