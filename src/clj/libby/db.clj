13a6f4eccf4951a96fe4c599e7055d45e88ba90f
Elliott Williams <elliottw@gmail.com>
2017-01-17 13:40:38 -0500

    trying to fix fucked up commits

--- src/clj/libby/db.clj
+++ src/clj/libby/db.clj
@@ -1,6 +1,5 @@
 (ns libby.db
-  (:require [clojure.java.jdbc :as j]
-            [clojure.core.memoize :as memo]))
+  (:require [clojure.java.jdbc :as j]))
 
 (def mysql-db {:dbtype "mysql"
                :dbname "bookwarrior"
@@ -10,11 +9,6 @@
 (defn do-sql [search]
   (j/query mysql-db ["select * from updated where title like ?" (str "%" search "%")]))
 
-(defn get-all-unique-titles [search]
-  (j/query mysql-db ["select title from updated"]))
-
-;;(defn get-all-unique-titles
-  ;;(j/query mysql-db ["select distinct title from updated"]))
 
 (defn- remove-an-empty [m] ;; http://stackoverflow.com/a/3938151
   (into {} (remove (comp #(= "" %) second) m)))
@@ -38,39 +32,4 @@
       fix-coverurls
       assoc-download-links))
 
-(do-sql "tidying")
-(def all-titles
-  (map :title (get-all-unique-titles "tidying")))
-
-(defn search->single-download-link [search]
-  (-> search
-      search->big-map
-      first
-      :download-link))
-
-(declare min-edit-distance edit-distance)
-
-(defn- edit-distance* [t p]
-  (cond
-   (empty? t) (count p)
-   (empty? p) (count t)
-   (= (last t) (last p)) (min-edit-distance t p 0)
-   :else (min-edit-distance t p 1)))
-
-
-(defn- min-edit-distance [t p cost]
-  (min (+ (edit-distance (butlast t) p) 1)
-       (+ (edit-distance t (butlast p)) 1)
-       (+ (edit-distance (butlast t) (butlast p)) cost)))
-
-
-(def edit-distance
-  "Get minimum distance between two strings"
-  (memo/lu edit-distance* :lu/threshold 50000))
-
-
-(defn search [word lst & {:keys [rank] :or {rank 3}}]
-  "Get a list of words based on the minimum distance"
-  (sort-by #(edit-distance word %) < (filter #(<= (edit-distance word %) rank)
-                                             lst)))
 
